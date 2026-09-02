cb6c31516f2c1015f742bf4c2fc81995d17fe49b
Daniels Kuzmins <daniels.kuzmins04@gmail.com>
2026-09-02 15:50:54 +0200

    Added navbar interaction

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// This is JS
+const navBar = document.querySelector("nav");
+const navLinks = Array.from(navBar.querySelectorAll("a"));
+const currentPage = window.location.pathname;
+
+navLinks.forEach((link) => {
+  const linkPage = link.getAttribute("href");
+
+  if (currentPage.includes(linkPage)) {
+    link.classList.remove("hover:bg-slate-800", "text-slate-300");
+    link.classList.add("bg-indigo-600", "text-white", "hover:bg-indigo-500");
+  }
+});
